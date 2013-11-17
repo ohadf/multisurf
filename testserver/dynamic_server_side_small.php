9c4ae17e088acddd4e980cf50709f4c70bc723dc
Ohad Fried <ohadfried@gmail.com>
2013-11-17 13:55:36 -0500

    added test website

---
+++ testserver/dynamic_server_side_small.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ This is some static text.
+ <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo rand() . "\n"; ?>
+ <br><br>
+ This is some more static text.
+ </body>
+</html>
+
