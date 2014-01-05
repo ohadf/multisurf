d806ef9ccf27d5c36ff8f48a27a8b3a8881fdafa
ae243 <ae243@cornell.edu>
2014-01-05 11:01:27 -0500

    updated crawler

--- crawl.py
+++ crawl.py
@@ -12,15 +12,15 @@ def simplecrawl():
 def crawl():
 
     alexa_sites = []
-    with open('top-1m.csv', 'rb') as f:
+    with open('top-1m.csv', 'rU') as f:
         reader = csv.reader(f)
         r = list(reader)
-        for item in r:
+        for position in range(67,501):
             # change to 501 for Alexa 500
-            if item[0] == '10':
-                break
-            else:
-                alexa_sites.append("www."+item[1])
+            #if item[0] == '501':
+            #    break
+            #elif r.index(item) >= 36 or r.index(item) <= 501:
+            alexa_sites.append("www."+r[position][1])
 
     safe = 0
     unsafe = 0
@@ -30,6 +30,7 @@ def crawl():
         print "starting client for: "+s
         result = basic_multisurf_client.doCrawl(s, 'localhost', 12345)
         print "finishing client for: "+s
+        print result
         if result == 2:
             print s+" : safe"
             safe += 1
@@ -38,7 +39,8 @@ def crawl():
             unsafe += 1
         else:   
             print s+" : error"
-            errors += 1    
+            errors += 1 
+        print str(safe)+" "+str(unsafe)+" "+str(errors)   
     return [safe,unsafe,errors]
 
 
