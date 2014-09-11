2bf81ea193cf1ebc905ee60fc740a43079152bbb
ae243 <annee@cs.princeton.edu>
2014-09-10 21:13:45 -0400

    added crawl script (initial stage)

--- crawls/crawl_individual.py
+++ crawls/crawl_individual.py
@@ -1,15 +1,56 @@
-# python crawl for us
+# usage: python crawl_individual.py <crawl_id>
 
-# crawl_id | time | thread_id | url | request | response_header | response_body 
+import requests
+import sys
+import time
+import httplib
+from threading import Thread
+import csv
 
-# 100 websites
+# get 100 sites from alexa top sites
+def get_sites():
+    alexa_sites = []
+    with open('top-1m.csv', 'rb') as f:
+        reader = csv.reader(f)
+        r = list(reader)
+        for item in r:
+            # change to 501 for Alexa 500
+            if item[0] == '101':
+                break
+            else:
+                alexa_sites.append("www."+item[1])
+    return alexa_sites
 
-# no response = -1
+def make_req(u, n):
+    # info for us
+    response = requests.get('http://'+u)
+    crawl_id = sys.argv[1]
+    t = time.time()
+    thread_id = n
+    url = response.url
+    req_headers = response.request.headers.items()
+    stat_code = response.status_code
+    headers = response.headers
+    history = response.history
+    body = response.text
+    print url, thread_id
 
-# mysql database
+    # TODO: info for peer
+        # 1. send request to peer
+        # 2. wait for response
+        # 3. get all info from response
 
-# number of visits per site: 3 (1 hour time interval), 5 (30 minute time interval), 10 (5 minute time interval)
 
-# number of peers = 0
 
-# Keep track of parameters: Day | Time interval | # of peers | # of sites visited | location | # of visits per site
+######## Start script ########
+
+sites = get_sites()
+
+# starts a new thread for each site
+count = 1
+for s in sites:
+    t = Thread(target=make_req, args=(s,count,))
+    t.start()
+    count += 1
+
+
