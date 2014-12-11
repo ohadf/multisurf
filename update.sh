8643022aaf2325e98278d6e49af5bcc459bd48af
masomel <msmelara@gmail.com>
2014-12-11 17:17:47 -0500

    Added update script

---
+++ update.sh
@@ -0,0 +1,10 @@
+yes | sudo yum update
+yes | sudo yum install make automake gcc gcc-c++ kernel-devel
+yes | sudo yum groupinstall “Development tools”
+yes | sudo yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
+wget http://ftp.gnu.org/gnu/tar/tar-1.28.tar.gz
+tar -xzvf tar-1.28.tar.gz
+cd tar-1.28
+./configure
+make
+sudo make install
