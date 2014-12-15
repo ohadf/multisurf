114285df9bcad3a68c33119770a1c6e1eea5605b
Marcela M <msmelara@gmail.com>
2014-12-15 17:13:37 -0500

    Deployed ssh key to all nodes

--- crawls/master_crawl.py
+++ crawls/master_crawl.py
@@ -25,23 +25,11 @@ client_id = sys.argv[2]
 user = sys.argv[1]
 pwd = getpass.getpass()
 
-timestamp = get_current_time_millis()
-
-# crawl 1: 1 second time interval, 300 sites
-thread = threading.Thread(target=start_crawl, args=(crawl_id, 1, 300, user, pwd, client_id, timestamp))
+# crawl 1: 300 seconds between requests, 300 sites (format of args: <crawl_id> <time interval between requests in seconds> <# of sites visited> <# of peers>)
+thread = threading.Thread(target=start_crawl, args=(crawl_id, 300, 300, user, pwd, client_id))
 thread.start()
 
-# crawl 2: 60 seconds between requests, 300 sites (format of args: <crawl_id> <time interval between requests in seconds> <# of sites visited> <# of peers>)
+# crawl 2: two peers on local machine, 600 seconds between requests, 300 sites (format of args: <crawl_id> <time interval between requests in seconds> <# of sites visited> <# of peers>)
 crawl_id += 1
-thread = threading.Thread(target=start_crawl, args=(crawl_id, 60, 300, user, pwd, client_id, timestamp))
-thread.start()
-
-# crawl 3: 300 seconds between requests, 300 sites (format of args: <crawl_id> <time interval between requests in seconds> <# of sites visited> <# of peers>)
-crawl_id += 1
-thread = threading.Thread(target=start_crawl, args=(crawl_id, 300, 300, user, pwd, client_id, timestamp))
-thread.start()
-
-# crawl 4: two peers on local machine, 600 seconds between requests, 300 sites (format of args: <crawl_id> <time interval between requests in seconds> <# of sites visited> <# of peers>)
-crawl_id += 1
-thread = threading.Thread(target=start_crawl, args=(crawl_id, 600, 300, user, pwd, client_id, timestamp))
+thread = threading.Thread(target=start_crawl, args=(crawl_id, 600, 300, user, pwd, client_id))
 thread.start()
