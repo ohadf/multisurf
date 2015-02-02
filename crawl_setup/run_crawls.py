225a342729c4d7aca984b4171c7e2676fe228258
masomel <msmelara@gmail.com>
2015-02-01 21:31:46 -0500

    cycles actually starts the crawl on the remote node

--- crawl_setup/run_crawls.py
+++ crawl_setup/run_crawls.py
@@ -2,6 +2,7 @@
 
 import sys
 import paramiko
+import select
 from threading import Thread, current_thread
 
 # get list of nodes
@@ -22,18 +23,38 @@ def get_nodes():
 
 # runs the crawl on the given node
 def remote_crawl(crawl_id, timeout, num_sites, node, freq):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
-    print ("Starting crawl type "+str(crawl_id)+" on node "+node)
-    client.connect(node, username='princeton_multisurf')
+    try: 
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        client.connect(node, username='princeton_multisurf')
+    except:
+        print("Could not connect to node "+node)
+        sys.exit(1)
 
+    #Send the command
+    stdin, stdout, stderr = client.exec_command('cd ./crawls; python collect.py '+str(crawl_id)+' '+str(timeout)+' '+str(num_sites)+' '+run_name+' '+node+' '+str(freq))
+
+    print ("Starting crawl type "+str(crawl_id)+" on node "+node)
+
+    f = open(node+'_crawl.log', 'ab+')
+
+    # wait for the command to terminate
+    while not stdout.channel.exit_status_ready():
+        if stdout.channel.recv_ready():
+            r1, w1, x1 = select.select([stdout.channel], [], [], 0.0)
+            if len(r1) > 0:
+                f.write(stdout.channel.recv(1024))
+
+    '''
     channel = client.invoke_shell()
     channel.send('cd ./crawls\n')
     out = ''
     while not out.endswith('$ '):
         resp = channel.recv(1024)
         out += str(resp)
+    
+    print ("Starting crawl type "+str(crawl_id)+" on node "+node)
 
     # Reading the output back seems to be the only way to 
     # make sure the update finishes
@@ -45,11 +66,13 @@ def remote_crawl(crawl_id, timeout, num_sites, node, freq):
         out += str(resp)
 
     # write the crawler's output to a log file, just for sanity
-    f = open(node+'_crawl.log', 'wb')
+    f = open(node+'_crawl.log', 'w+')
     f.write(out)
     f.close()
+    '''
 
     client.close()
+    f.close()
     print ("Finished crawl type "+str(crawl_id)+" on node "+node)
 
 nodes = get_nodes()
